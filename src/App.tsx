deac4da1d7569cc5ab33fd8a20d62c1eee2a287d
Jewoo <jeus0630@designfever.com>
2022-02-27 01:34:10 +0900

    Docs: Watch.tsx
    Added Watch Video

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/*" element={<Home></Home>}></Route>
+        <Route path="/*" element={<Watch></Watch>}></Route>
       </Routes>
     </Router>
   );
