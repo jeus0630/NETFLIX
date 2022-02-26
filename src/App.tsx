3d999827d27d4a97840d45c736cbea93cd5b2bb8
Jewoo <jeus0630@designfever.com>
2022-02-27 01:25:44 +0900

    folder structure changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
