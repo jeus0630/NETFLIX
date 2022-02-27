9829d45c48448e75bcb80810f876f134cd2becf0
Jewoo <jeus0630@designfever.com>
2022-02-28 01:04:36 +0900

    Update: lists.js
    List CURD

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/users');
 const movieRoute = require('./routes/movies');
+const listRoute = require('./routes/lists');
 
 app.use(helmet());
 app.use(morgan('dev'));
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",usersRoute); 
 app.use("/api/movies",movieRoute); 
+app.use("/api/lists",listRoute);
 
 const main = () => {
     app.listen(process.env.PORT || 8080, () => {
