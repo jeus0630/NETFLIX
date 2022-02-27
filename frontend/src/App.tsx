736514ea47fd3c8f663787c2d9bdd017e4d380bb
Jewoo <jeus0630@designfever.com>
2022-02-28 02:30:39 +0900

    Update: App.tsx, Home.tsx
    React-Router-Dom Conditional rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,17 @@ import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 function App() {
+  const user = true;
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/" element={user ? <Home></Home> : <Register></Register>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={!user ? <Login></Login> : <Home></Home>}></Route>
+        <Route path="/series" element={user? <Home type={"series"}></Home> : <Login></Login>}></Route>
+        <Route path="/movies" element={user? <Home type={"movie"}></Home> : <Login></Login>}></Route>
+        <Route path="/watch" element={user? <Watch></Watch> : <Login></Login>}></Route>
         <Route path="/*" element={<Login></Login>}></Route>
       </Routes>
     </Router>
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -3,13 +3,15 @@ import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
-interface IHomeProps {}
+interface IHomeProps {
+  type?: string;
+}
 
-const Home: React.FunctionComponent<IHomeProps> = (props) => {
+const Home: React.FunctionComponent<IHomeProps> = ({ type }) => {
   return (
     <div className="home">
       <Navbar></Navbar>
-      <Featured type="movie"></Featured>
+      <Featured type={type}></Featured>
       <List></List>
     </div>
   );
