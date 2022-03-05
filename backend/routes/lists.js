b53c8ff6a29969a0c2aeadf9972937144a807bfc
Jewoo <jeus0630@designfever.com>
2022-03-05 23:26:46 +0900

    Update: lists.js
    update PUT method added

--- backend/routes/lists.js
+++ backend/routes/lists.js
@@ -62,4 +62,20 @@ router.get("/",verify, async(req,res)=>{
     }
 })
 
+//UPDATE
+router.put("/:id", verify, async (req,res)=>{
+    if(req.user.isAdmin){
+        try{
+            const updatedLists = await List.findByIdAndUpdate(req.params.id,{
+                $set: req.body
+            },{new: true});
+            res.status(201).json(updatedLists);
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json("You are not allowed");
+    }
+});
+
 module.exports = router;
