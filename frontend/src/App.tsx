1ba7601c0b34e37da0218f71324a2235e2268af4
Jewoo <jeus0630@designfever.com>
2022-03-06 15:15:08 +0900

    Fix: App.tsx
    Redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -7,17 +7,17 @@ import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 function App() {
-  const user = true;
+  const user = false;
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={user ? <Home></Home> : <Register></Register>}></Route>
+        <Route path="/" element={user ? <Home></Home> : <Navigate replace to="/login"></Navigate>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
-        <Route path="/login" element={!user ? <Login></Login> : <Home></Home>}></Route>
-        <Route path="/series" element={user? <Home type={"series"}></Home> : <Login></Login>}></Route>
-        <Route path="/movies" element={user? <Home type={"movie"}></Home> : <Login></Login>}></Route>
-        <Route path="/watch" element={user? <Watch></Watch> : <Login></Login>}></Route>
+        <Route path="/login" element={!user ? <Login></Login> : <Navigate replace to="/login"></Navigate>}></Route>
+        <Route path="/series" element={user ? <Home type={"series"}></Home> : <Navigate replace to="/login"></Navigate>}></Route>
+        <Route path="/movies" element={user ? <Home type={"movie"}></Home> : <Navigate replace to="/login"></Navigate>}></Route>
+        <Route path="/watch" element={user ? <Watch></Watch> : <Navigate replace to="/login"></Navigate>}></Route>
         <Route path="/*" element={<Login></Login>}></Route>
       </Routes>
     </Router>
